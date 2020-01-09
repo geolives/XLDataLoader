520b52e52d96eb5781485fde45366686bdbc9d64
Doubotis <cbrasseur@mbpdechistophe2.geolives.net>
2020-01-09 13:51:17 +0100

    Support for iOS 13 buids
    Removing support for AFNetworking UIWebView.

---
+++ XLDataLoader/AFNetworking.h
@@ -0,0 +1,19 @@
+//
+//  AFNetworking.h
+//  AFNetworking
+//
+//  Created by Christophe on 09/01/2020.
+//
+
+#import "AFHTTPRequestOperation.h"
+#import "AFHTTPRequestOperationManager.h"
+#import "AFURLConnectionOperation.h"
+
+#import "AFHTTPSessionManager.h"
+#import "AFURLSessionManager.h"
+
+#import "AFNetworkReachabilityManager.h"
+#import "AFSecurityPolicy.h"
+
+#import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
